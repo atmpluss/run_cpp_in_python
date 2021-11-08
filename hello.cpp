3713386e2d82abe58f6442dbfdc65c79724376e6
Sanayei <mohammad.sanayei@bam.de>
2021-11-08 15:55:22 +0100

    add txt output

--- hello.cpp
+++ hello.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
 int add(int a, int b){
     return a + b;
 }
 int main(){
     for( int ii=0; ii<10; ++ii ){
+        int i;
         int input1;
         int input2;
         std::vector<int> c;
-        std::cin >> input1>>input2;
+        std::cin >>i >> input1>>input2;
         int result = add(input1, input2);
         c.push_back(result);
+        std::ofstream myfile;
+        std::string name = "example" + std::to_string(i) +".txt";
+        myfile.open (name);
+        myfile << "output: "<<result<<"\n";
+        myfile.close();
         std::cout << c[0] << std::endl;
         std::cout.flush();
+        //return 0;
     }
 }
--- main.py
+++ main.py
@@ -3,16 +3,19 @@ from random import randint
 
 p = Popen(['a.exe'], shell=True, stdout=PIPE, stdin=PIPE)
 for ii in range(10):
-    value = str(randint(0, 10)) + '\n'
-    value = bytes(value, 'UTF-8')  # Needed in Python 3.
+    value0 = str(ii) + '\n'
+    value0 = bytes(value0, 'UTF-8')  # Needed in Python 3.
+
+    value1 = str(randint(0, 10)) + '\n'
+    value1 = bytes(value1, 'UTF-8')  # Needed in Python 3.
     value2 = str(randint(0, 10)) + '\n'
     value2 = bytes(value2, 'UTF-8')  # Needed in Python 3.
 
-
-    p.stdin.write(value)
+    p.stdin.write(value0)
+    p.stdin.write(value1)
     p.stdin.write(value2)
     p.stdin.flush()
     result = p.stdout.readline().strip()
     print("c++ result: " + str((result)))
-    print("python result: " +str(float(value) + float(value2)))
+    print("python result: " +str(float(value1) + float(value2)))
     print("+++++++++++++++++++++++++++++++++++++++++++++++++++++")
